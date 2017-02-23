91a1c0f0119264f7ecaf46ecf07212e75f40241a
Booker <bgong001c@cable.comcast.com>
2017-02-23 13:49:01 -0500

    fix sum pattern match style

--- src/main/scala/ch2/exhaustive/Sum.scala
+++ src/main/scala/ch2/exhaustive/Sum.scala
@@ -5,13 +5,15 @@ package ch2.exhaustive
 object Sum {
 
   // O(2^n)
+  // boolean style
   def dfs(a: Array[Int], k: Int): Boolean =
     (a.isEmpty && k == 0) ||
       (a.nonEmpty &&
         (dfs(a.tail, k) || dfs(a.tail, k - a.head)))
 
+  // pattern match style
   def dfs(a: List[Int], k: Int): Boolean = a match {
-      case Nil if k==0 => true
+      case Nil => k==0
       case x => dfs(a.tail, k) || dfs(a.tail, k - a.head)
     }
 
