2f63ccc6e536c5764808c54f600907dc31d14cd8
Booker <bgong001c@cable.comcast.com>
2017-02-24 17:41:23 -0500

    LIS

---
+++ src/main/scala/ch2/dynamic/LIS.scala
@@ -0,0 +1,38 @@
+package ch2.dynamic
+
+// Longest increasing subsequence
+
+object LIS {
+
+  // solution 1
+  // dp[i] = the length of LIS that ends with a_i
+  // dp[i] = max { 1, dp[j]+1 if j<i & aj<ai }
+
+  // O(n^2)
+  def solve(a:Array[Int]) = {
+    val dp = Array.fill(a.length)(1)
+    for (i <- a.indices; j <- 0 until i if a(j)<a(i))
+      dp(i) = math.max(dp(i), dp(j)+1)
+    dp.max
+  }
+
+  // solution 2
+  // dp[i] = min of tail element of i+1 length LIS
+  // for all j, if a_j>dp[i-1], then assign dp[i]=min(dp[i],a_j)
+  //         dp is automatically sorted
+  //         actually the first available is what we want, no further update needed
+  // change assign order, as each a_j only assign once
+  //         use binary search instead => O(nlogn)
+  //         find the first i allow dp[i] smaller than current a_j, and replace it ( usually infinity )
+  def solve2(a:Array[Int]) = {
+    val dp = Array.fill(a.length)(Double.PositiveInfinity)
+    for (i <- a.indices)
+      dp(lowerBound(dp, a(i))) = a(i)
+    dp.view.zipWithIndex.filter(!_._1.isInfinity).last._2
+  }
+
+  def lowerBound(k:Array[Double], target:Int):Int = {
+    // binary search, return first element >= target
+    1
+  }
+}
--- src/test/scala/test.sc
+++ src/test/scala/test.sc
@@ -1,23 +1,7 @@
 
+val a = Array(1,41,56,3,32,352,6,2,1,241,124,123,1,515151)
 
-
-  val w = Array(2,1,3,2)
-  val v = Array(3,2,4,2)
-  val W  = 5
-
-  val max_v = v.max
-  val l = w.length
-  val dp = Array.fill(l+1)(Array.fill(l*max_v+1)(Double.PositiveInfinity))
-  dp(0)(0) = 0
-
-  // the minimum wei
-  for (i <- 0 until l; j <- 0 to l*max_v) {
-    if (j < v(i)) dp(i+1)(j) = dp(i)(j)
-    else dp(i+1)(j) = math.min(dp(i)(j), dp(i)(j - v(i)) + w(i))
-  }
-
-  dp(l).view.zipWithIndex
-    .filter(_._1<=W)
-    .filter(!_._1.isInfinity)
-    .last._2
-
+  val dp = Array.fill(a.length)(1)
+  for (i <- a.indices; j <- 0 until i if a(j)<a(i))
+      dp(i) = math.max(dp(i), dp(j)+1)
+  dp.max
