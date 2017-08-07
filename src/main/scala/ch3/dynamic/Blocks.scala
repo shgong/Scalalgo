c2b547f3dcb6a8ec8407cf57cab74ec369eee4f9
Booker <bgong001c@cable.comcast.com>
2017-08-07 15:06:43 -0400

    blocks

---
+++ src/main/scala/ch3/dynamic/Blocks.scala
@@ -0,0 +1,19 @@
+package ch3.dynamic
+
+object Blocks {
+
+  // Optimized Pavement
+  def pave(color: Array[Array[Boolean]]): Int = {
+
+    1
+  }
+
+  def main(args: Array[String]): Unit = {
+    val a = Array(
+      Array(false, false, false),
+      Array(false, true, false),
+      Array(false, false, false)
+    )
+    println(pave(a))
+  }
+}
