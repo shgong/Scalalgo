c931cfbc91c40d1c64f4ff37aa07a03af96265ed
Booker <bgong001c@cable.comcast.com>
2017-08-07 17:29:19 -0400

    blocks

--- src/main/scala/ch3/dynamic/Blocks.scala
+++ src/main/scala/ch3/dynamic/Blocks.scala
@@ -2,18 +2,36 @@ package ch3.dynamic
 
 object Blocks {
 
-  // Optimized Pavement
-  def pave(color: Array[Array[Boolean]]): Int = {
+  // N blocks, paint with R B G Y
+  // how many plan have even R and even G
 
-    1
+  def solve(N: Int): Int = {
+    /* math problem
+       ai  RG even
+       bi  one of RG even
+       ci  RG odd
+
+       ai+1 = 2ai + bi
+       bi+1 = 2ai + 2bi + 2ci
+       ci+1 = bi + 2ci
+
+       ai   |2 1 0| i |1|
+       bi = |2 2 2|   |0|
+       ci   |0 1 2|   |0|
+    */
+
+    var (a,b,c) = (1,0,0)
+    for (i <- 1 to N) {
+      a = 2 * a + b
+      c = 2 * c + b
+      b = a + c
+    }
+    a
   }
 
   def main(args: Array[String]): Unit = {
-    val a = Array(
-      Array(false, false, false),
-      Array(false, true, false),
-      Array(false, false, false)
-    )
-    println(pave(a))
+
+    println(solve(1)) //2
+    println(solve(2)) //6   RR BB BY YB YY GG
   }
 }
