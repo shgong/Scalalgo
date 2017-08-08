ebb9a489903495d563438633b70874fa6a937fe5
Booker <bgong001c@cable.comcast.com>
2017-08-08 17:24:14 -0400

    wait for segment tree

--- src/main/scala/ch3/dynamic/MinimizingMaximizer.scala
+++ src/main/scala/ch3/dynamic/MinimizingMaximizer.scala
@@ -1,5 +1,7 @@
 package ch3.dynamic
 
+import scala.collection.mutable
+
 object MinimizingMaximizer {
 
   /*
@@ -9,19 +11,37 @@ object MinimizingMaximizer {
     Find the min subset (in the same order) that still works
   */
 
-  def solve(stPairs: List[(Int,Int)]): List[Int] = {
-    val (s,t) = stPairs.unzip
+  def solve(stPairs: List[(Int,Int)]) = {
 
     // must sort smaller part first, then cover last of previous smaller part
     // just consider extreme case, first is max
-    s
+
+    /* stack is wrong can not throw away, need dp
+    val stack = mutable.Stack((0,1))
+    for((s,t)<-stPairs){
+      if((t>stack.top._2 && s<=stack.top._2) || (t==stack.top._2 && s<=stack.top._1)) {
+        while (stack.top._1 >= s) stack.pop()
+        stack.push((s,t))
+      }
+    }
+    stack.reverse
+    */
+
+    // segment tree ?
+
 
   }
 
   def main(args: Array[String]): Unit = {
 
-    println(solve(
-      List((20,30),(1,10),(10,20),(20,30),(15,25),(30,40))
-    ))
+    val testCases = List(
+      List((20,30),(1,10),(10,20),(20,30),(15,25),(30,40)),
+      List((20,30),(1,10),(10,20),(20,30),(15,25),(30,40)),
+      List((20,30),(1,10),(10,20),(15,25),(20,30),(15,25),(25,30),(30,40)),
+      List((20,30),(1,10),(1,20),(20,30),(15,25),(30,40))
+    )
+
+    testCases.map(x=> println(solve(x)))
+
   }
 }
