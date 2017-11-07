644f8e525cac5a46f0d5bd3e72d7b0e245e18808
Booker <bgong001c@cable.comcast.com>
2017-11-07 17:06:55 -0500

    DNA

---
+++ src/main/scala/ch4/str/DNA.scala
@@ -0,0 +1,28 @@
+package ch4.str
+
+// given S, find how many length n string don't include S
+
+object DNA {
+
+  // use dp, keep last 3 char as state
+  // like S = ATCG, there is 4^3 state
+
+  // state compression
+  // A, AT, ATC, other
+  // when S is longer
+  // A, AT, ATC, ATCA, ATCAT, ATCATC, first belong to longer state
+
+  // use state table to do DP
+
+  def solve(n:Int, S:String): Int = {
+
+    1
+  }
+
+
+  def main(args:Array[String]) = {
+    val res = solve(n=3, S="AT")
+    println(res)
+  }
+
+}
---
+++ src/main/scala/ch4/str/Sequence.scala
@@ -0,0 +1,5 @@
+package ch4.str
+
+object Sequence {
+
+}
