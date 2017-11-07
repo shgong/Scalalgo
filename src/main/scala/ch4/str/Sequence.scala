9f3a2409ac6b1285a0eaf489921f76095ee9e4da
Booker <bgong001c@cable.comcast.com>
2017-11-07 17:10:48 -0500

    suffix array

--- src/main/scala/ch4/str/Sequence.scala
+++ src/main/scala/ch4/str/Sequence.scala
@@ -1,5 +1,24 @@
 package ch4.str
 
 object Sequence {
+  // Array A, first element is max
+  // split into three sequence, and inverse them
+  // what is the smallest inversed sequence in (dict order)
+
+  // example 10,1,2,3,4
+  // into 10,1 - 2 - 3,4
+  // after inverse 1,10,2,4,3
+
+
+  def solve(n:Int, S:String): Int = {
+
+    1
+  }
+
+
+  def main(args:Array[String]) = {
+    val res = solve(n=3, S="AT")
+    println(res)
+  }
 
 }
