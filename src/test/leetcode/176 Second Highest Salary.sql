5106cc0b66eb72a962140c5a43d7553dc6f6b7bc
Booker <bgong001c@cable.comcast.com>
2018-01-10 16:51:58 -0500

    176

---
+++ src/test/leetcode/176 Second Highest Salary.sql
@@ -0,0 +1,2 @@
+# Write a SQL query to get the second highest salary from the Employee table.
+SELECT MAX(Salary) AS SecondHighestSalary FROM Employee WHERE Salary!=(SELECT MAX(Salary) FROM Employee)
